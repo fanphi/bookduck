e05942995b7438d132501946047503c8d7080e9f
fanphi <fannyp90@hotmail.com>
2022-04-15 23:59:30 +0200

    added books to profile page

--- frontend/profile.js
+++ frontend/profile.js
@@ -4,6 +4,7 @@ let loginContainer = document.querySelector(".login");
 let loggedIn = document.querySelector(".logged-in");
 let loggedInPara = document.querySelector(".login-para");
 let userContainer = document.querySelector("#user-info");
+let profileBooks = document.querySelector("#profile-books");
 
 const checkUser = async () => {
     let response = await axios.get("http://localhost:1337/api/users/me",
@@ -14,19 +15,85 @@ const checkUser = async () => {
     })
     console.log(response);
     let createdAt = response.data.createdAt;
+    let id = response.data.id;
     // createdAt.toISOString();
    console.log( createdAt.slice(0,10));
     
     loggedInPara.innerText = "Welcome " + response.data.username;
-    userContainer.innerHTML = `<p>Username: ${response.data.username} <br>
+    userContainer.innerHTML = `<h2>My Info</h2><p>Username: ${response.data.username} <br>
     Email: ${response.data.email}<br>
-    User ID: ${response.data.id}<br>
-    Created: ${createdAt.slice(0,10)}</p><br>
-    <h2>My Books</h2>`
+    User ID: ${id}<br>
+    Created: ${createdAt.slice(0,10)}</p><br>`
+
+    let renderBooks = async () => {
+        let bookResponse = await axios.get("http://localhost:1337/api/books?populate=*");
+        let book = bookResponse.data.data;
+        console.log(book);
+    
+        book.forEach(book => {
+            if(id == book.attributes.users_permissions_user.data.id){
+            let title = document.createElement("p");
+            let author = document.createElement("p");
+            let pages = document.createElement("p");
+            let review = document.createElement("p");
+            let username = document.createElement("p");
+            let email = document.createElement("p");
+            title.innerText = "Title: " + book.attributes.title;
+            author.innerText = "Author: " + book.attributes.author;
+            pages.innerText = "Number of pages: " + book.attributes.pages;
+            review.innerText = "Review: " + book.attributes.review + "/10";
+            username.innerText = "Username: " + book.attributes.users_permissions_user.data.attributes.username;
+            email.innerText = "Email: " + book.attributes.users_permissions_user.data.attributes.email;
+    
+            let cover = document.createElement("img");
+            if(book.attributes.cover.data) {
+            cover.src = "http://localhost:1337" + book.attributes.cover.data.attributes.url;
+            }
+    
+    
+            profileBooks.append(cover,title,author,pages, review, username, email);
+        }
+        });
+    }
+
+    let renderAudio = async () => {
+        let audioResponse = await axios.get("http://localhost:1337/api/audios?populate=*");
+        let audio = audioResponse.data.data;
+        console.log(audio);
+     
+    audio.forEach(audio => {
+        if(id == audio.attributes.users_permissions_user.data.id){
+            console.log(audio.attributes.users_permissions_user.data.id)
+            let title = document.createElement("p");
+            let minutes = document.createElement("p");
+            let review = document.createElement("p");
+            let published = document.createElement("p");
+            let username = document.createElement("p");
+            let email = document.createElement("p");
+    
+            title.innerText = "Title: " + audio.attributes.title;
+            minutes.innerText = "Minutes: " + audio.attributes.minutes;
+            review.innerText = "Review: " + audio.attributes.review + "/10";
+            published.innerText = "Publishing date: " + audio.attributes.published;
+            username.innerText = "Username: " + audio.attributes.users_permissions_user.data.attributes.username;
+            email.innerText = "Email: " + audio.attributes.users_permissions_user.data.attributes.email;
+    
+            let cover = document.createElement("img");
+            if(audio.attributes.cover.data) {
+            cover.src = "http://localhost:1337" + audio.attributes.cover.data.attributes.url;
+            }
+    
+    
+            profileBooks.append(cover,title,minutes, review, published, username, email);
+        }
+        });
+    }
 
 if(data != null){
 loggedIn.style.display = "block";
 }
+renderBooks();
+renderAudio();
 
 }
 
@@ -44,3 +111,4 @@ logoutBtn.addEventListener("click", ()=>{
     sessionStorage.clear();
     window.location.href = "./index.html"  
 })
+
